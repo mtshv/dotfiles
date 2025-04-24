c3236fa34fe2304b9e1d7affc4792869b76f615a
mtshv <i.mityushev@gmail.com>
2025-04-24 18:59:08 +0200

    remove test fresh2.sh file

--- fresh2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "Setting up your Mac..."
-
-# Check for Oh My Zsh and install if we don't have it
-if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
